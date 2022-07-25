28ed353419b9fb8e96ea4e1d2f1b4a7093ee77fc
ayush gupta <68463315+ayush-ji@users.noreply.github.com>
2022-07-25 00:47:54 +0000

    DATABASE CONNECTED AND ERRORS FIXED

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const env = process.env;
-
+const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose')
+
+var resu = 
+mongoose.connect('mongodb+srv://ayushji:Ayush16375@cluster0.oaoyg.mongodb.net/?retryWrites=true&w=majority').then( r => { console.log("done!") });
+
+
 
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello From Nodejs ji')
+  res.render('screens/home');
 })
 
 app.listen(env['PORT'], () => {
   console.log('APP STARTED');
+  console.log(resu)
 })
